7bbeba9fc3005e4dc045dd0d2d57d909eca9d4fb
Edward-Code007 <glezmoradev@gmail.com>
2026-06-05 21:54:39 -0400

    Initial

---
+++ CancellationTokenExample.cs
@@ -0,0 +1,45 @@
+namespace CancellationTokenExample;
+
+public class CancellationToken
+{
+    public static void CancelTask()
+    {
+        CancellationTokenSource sourceToken = new CancellationTokenSource();
+        sourceToken.CancelAfter(20);//Cambiar Valor Altera el Resultado del Task
+
+
+        var task = Task.Factory.StartNew(() =>
+        {
+            int j = 0;
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                sourceToken.Token.ThrowIfCancellationRequested();
+                int childNUmber = j;
+                Task.Factory.StartNew(() =>
+        {
+                Console.WriteLine($"Log dentro del Task Child #{childNUmber}");
+            }, TaskCreationOptions.AttachedToParent);
+                j++;
+            }
+        }, sourceToken.Token);
+
+        task.ContinueWith(x =>
+        {
+            if (x.IsCompletedSuccessfully)
+            {
+                Console.WriteLine("Completado Exitosamente");
+            }
+            else if (x.IsCanceled)
+            {
+                Console.WriteLine("Cancelada");
+            }
+            else if (x.IsFaulted)
+            {
+                Console.WriteLine("Falló con excepción: " + x.Exception?.InnerException?.Message);
+            }
+        });
+
+        Console.ReadLine();
+
+    }
+}
---
+++ TaskCompletionExaple.cs
@@ -0,0 +1,41 @@
+namespace TaskCompletionExaple
+{
+    public class TaskCompletion
+    {
+
+       public static TaskCompletionSource<bool> TaskDeclaration()
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            Task task = tcs.Task;
+
+            task.ContinueWith(completed =>
+            {
+                if (completed.IsCompletedSuccessfully)
+                {
+                    Console.WriteLine("Se Completo Exitosamente");
+                }
+                else
+                {
+                    Console.WriteLine("Task Cancelada o Fallida");
+                }
+
+            });
+            return tcs;
+        }
+        public static void Run()
+        {
+            var tcs = TaskDeclaration();
+
+
+            while (true)
+            {
+            Console.WriteLine("1 para completar la tarea\n 2 para Cancelar");
+                var opt = Console.ReadLine();
+                if (opt == "1") { tcs.SetResult(true); }
+                if (opt == "2") { tcs.SetCanceled(); }
+            }
+        }
+
+    }
+
+}
